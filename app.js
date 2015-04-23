eb094bbe0fdbfc4e1da7fcbbf3618669fba33152
sherri <sherri@sherricruz.com>
2015-04-23 11:17:34 -0700

    created two new directives

--- app.js
+++ app.js
@@ -2,6 +2,32 @@
  * Created by SHERRI on 4/23/15.
  */
 ;(function(window){
-    angular.module('app', [])
+    angular.module('app', ['ui.bootstrap']);
+
+    //define directives
+  angular.module('app', [])
+.directive('tab', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    template: '<h2>hello</h2> <div role="tabpanel" ng-transclude></div>',
+    scope: { },
+    link: function(scope, elem, attr) {}
+  }
+})
+      .directive('tabset', function() {
+          return {
+              restrict: 'E',
+              transclude: true,
+              scope: { },
+              templateUrl: 'tabset.html',
+              controllerAs: 'tabset',
+              controller: function() {
+                  var self = this
+                  self.tabs = []
+
+              }
+          }
+      })
 
 })(window);
